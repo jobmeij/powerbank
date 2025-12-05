5a1b55b0f552f957cac8c37e2b3822e608731741
Job Meijer <jobmeijer@hotmail.com>
2025-12-05 16:09:02 +0100

    minor changes

--- MATLAB/Boost_StateSpace/boost_switching_state_space.m
+++ MATLAB/Boost_StateSpace/boost_switching_state_space.m
@@ -4,14 +4,14 @@ clear all; close all; clc;
 
 
 %% Boost converter parameters
-L  = 40e-6;       % Inductance [H]
+L  = 47e-6;       % Inductance [H]
 R_L = 0.1;        % Inductor resistance [Ohm]
-C  = 10e-6;       % Capacitance [F]
+C  = 1e-3;        % Capacitance [F]
 R  = 7;           % Load resistance [Ohm]
 Vin = 3.7;        % Input voltage [V]
-fsw = 500e3;      % Switching frequency [Hz]
-duty = 0.48;      % Duty cycle [-]
-t_final = 1e-3;   % Simulation end time [s]  
+fsw = 100e3;      % Switching frequency [Hz]
+duty = 0.5;      % Duty cycle [-]
+t_final = 5e-3;   % Simulation end time [s]  
 x0 = [0; 0];      % Initial conditions iL [A] and vC [V]
 % State vector: x = [iL; vC]
 
@@ -32,7 +32,7 @@ B_off = [1/L;
 
 %% Open loop simulation
 % Simulate for N switching periods
-opts = odeset('RelTol',1e-6,'AbsTol',1e-9);     % Tolerance options
+opts = odeset('RelTol',1e-9,'AbsTol',1e-9);     % Tolerance options
 [t, x] = ode45(@(t,x) boost_dyn(t,x,A_on,B_on,A_off,B_off,Vin,fsw,duty), [0 t_final], x0, opts);
 
 
@@ -40,25 +40,25 @@ opts = odeset('RelTol',1e-6,'AbsTol',1e-9);     % Tolerance options
 %% Plotting
 figure;
 hold all
-plot(t, x(:,1));
-plot(t, x(:,2)); 
+plot(t, x(:,1));    % iL
+plot(t, x(:,2));    % vC 
 grid on;
 xlabel('Time (s)');
 ylabel('States');
-legend('v_C','i_L');
+legend('i_L','v_C');
 title('Boost converter state-space switching simulation');
 
 
 %% Control simulation
 
-fs_control = 10e3;      % Control loop frequency
-ts_control = 1/fs_control;
-
-for i = 1:10
-    [tc, xc] = ode45(@(t,x) boost_dyn(t,x,A_on,B_on,A_off,B_off,Vin,fsw,duty), [0 t_final], x0, opts);
-    
-
-end
+% fs_control = 10e3;      % Control loop frequency
+% ts_control = 1/fs_control;
+% 
+% for i = 1:10
+%     [tc, xc] = ode45(@(t,x) boost_dyn(t,x,A_on,B_on,A_off,B_off,Vin,fsw,duty), [0 t_final], x0, opts);
+% 
+% 
+% end
 
 
 %% Differential equation function
